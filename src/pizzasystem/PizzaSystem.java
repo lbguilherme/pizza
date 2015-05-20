30c75b34d1be17ffb6adb3de6c03bcebde0e0f40
Guilherme Bernal <dev@lbguilherme.com>
2015-05-20 20:59:09 -0300

    Open login dialog on startup

--- src/pizzasystem/PizzaSystem.java
+++ src/pizzasystem/PizzaSystem.java
@@ -15,7 +15,7 @@ public class PizzaSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        pizzasystem.ui.Login.main(null);
     }
     
 }
