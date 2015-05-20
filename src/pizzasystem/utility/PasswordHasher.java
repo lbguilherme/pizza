0a9de82399d868d3fcc42a90ad27fce0bd3f2579
Guilherme Bernal <dev@lbguilherme.com>
2015-05-20 19:33:09 -0300

    Implement password hasher

--- src/pizzasystem/utility/PasswordHasher.java
+++ src/pizzasystem/utility/PasswordHasher.java
@@ -1,10 +1,32 @@
-
 package pizzasystem.utility;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PasswordHasher {
     
+    private MessageDigest digester;
+
+    public PasswordHasher() {
+        try {
+            digester = MessageDigest.getInstance("SHA-1");
+        }
+        catch (NoSuchAlgorithmException e) {
+            // hash won't be able to hash anything
+            digester = null;
+        } 
+    }
+    
     public String hash(String plainTextPassword) {
-        return "";
+        if (digester == null)
+            return plainTextPassword;
+        
+        byte[] bytes = digester.digest(plainTextPassword.getBytes());
+        String hash = "";
+        for (int i = 0; i < bytes.length; i++) {
+            hash += Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return hash;
     }
     
 }
